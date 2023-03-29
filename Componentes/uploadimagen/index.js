8b80ebef478d45bff6517516306d0005fcec9533
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-03-29 18:38:19 -0300

    components pt1,2,3

---
+++ Componentes/uploadimagen/index.js
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from "react";
+
+export function UploadImagem({
+    className = '',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName= '',
+    aoSetarAReferencia
+}) {
+
+    const referenciaInput = useRef(null);
+
+    useEffect(() => {
+        if (!aoSetarAReferencia){
+            return;
+        }
+
+        aoSetarAReferencia(referenciaInput?.current);
+    }, [referenciaInput?.current]);
+
+    const abrirSeletorArquivos = () =>{
+        referenciaInput?.current?.click();
+    }
+
+    const aoAlterarImagem = () =>{
+        console.log('alterar')
+
+        if(!referenciaInput?.current?.files?.length ){
+           return;
+        }
+
+        const arquivo = referenciaInput?.current?.files[0];
+
+        const leitorDoc = new FileReader();
+        leitorDoc.readAsDataURL(arquivo);
+        leitorDoc.onloadend =() => {
+            setImagem({
+                preview: leitorDoc.result,
+                arquivo
+            });
+        }
+    }
+
+    return (
+        <div className={`uploadImagemContainer ${className}`} onClick={abrirSeletorArquivos}>
+            {imagemPreview && (
+                <div className="imagemPreviewContainer">
+                    <img 
+                        src={imagemPreview}
+                        alt='imagemPreview'
+                        className={imagemPreviewClassName}
+                        />
+                </div>
+            )}
+
+            <input 
+                type='file' 
+                className="oculto" 
+                accept="image/*"
+                ref={referenciaInput}
+                onChange={aoAlterarImagem}
+            />
+        </div>
+    );
+}
