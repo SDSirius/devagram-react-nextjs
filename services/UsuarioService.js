8accec6134e6fd514189704c4bc56fd446911fd6
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-04-04 17:48:58 -0300

    Update UsuarioService.js

--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -1,9 +1,19 @@
 import HttpService from "./HttpService";
 
 export default class UsuarioService extends HttpService {
-    async login(usuario, senha) {
+    async login(credenciais) {
+        const { data} = await this.post('/login',credenciais);
+
+        localStorage.setItem("nome", data.nome);
+        localStorage.setItem("email", data.email);
+        localStorage.setItem("token", data.token);
+
+        if(data.avatar){
+            localStorage.setItem("avatar", data.avatar);
+        }
 
     }
+
     async cadastro(dados) {
         return this.post('/cadastro', dados);
     }
