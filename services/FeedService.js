c02a9f83535460b2dec3fb6b4f1bf04372288e7f
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-04-26 18:56:22 -0300

    Update FeedService.js

--- services/FeedService.js
+++ services/FeedService.js
@@ -2,15 +2,10 @@ import HttpService from "./HttpService";
 
 export default class FeedService extends HttpService {
     async loadPosts(idUser) {
-        console.log("iniciando a função loadPost");
         let url = '/feed';
-        console.log(`1 id user esta ${idUser} e url esta ${url}`);
         if (idUser) {
             url += `?id=${idUser}`;
-            console.log(`2 id user esta ${idUser} e url esta ${url}`);
         }
-
         return this.get(url);
-
     }
 }
